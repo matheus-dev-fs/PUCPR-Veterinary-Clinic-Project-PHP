0dc1a5b3c09ccaf81c55aabfcb83d3a5f1b22486
matheus-dev-fs <matheusmks10@gmail.com>
2025-11-15 10:23:54 -0300

    Add simple Database connection class for testing
    - Created a basic Database class in app/core to handle PDO connection.
    - This initial version is for testing purposes and will be improved in the future.

---
+++ app/core/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\core;
+
+class Database
+{
+    private \PDO $pdo;
+
+    public function connect(): \PDO
+    {
+        $host     = 'localhost';
+        $dbname   = 'clinica_db';
+        $username = 'root';
+        $password = '';
+        $charset  = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+        ];
+
+        try {
+            $this->pdo = new \PDO($dsn, $username, $password, $options);
+            return $this->pdo;
+        } catch (\PDOException $e) {
+            throw new \RuntimeException('Database connection failed: ' . $e->getMessage());
+        }
+    }
+}
